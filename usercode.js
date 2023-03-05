2ca7b01384cd8ce47fcf43cb5303891419cc8c7b
George <golubev_georgiy@ukr.net>
2023-03-05 19:57:36 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,4 +1,9 @@
+// build command: npm install crypto-js
+
+const sha1 = require("crypto-js/sha1");
+
 module.exports = (data) => {
-    data.hello = "Hello World!" + data.request;
+    //data.res = sha1(data.in).toString(); 
+     data.hello = "Hello World!" + data.in;
     return data;
 };
