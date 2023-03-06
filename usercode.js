6a4c298635295052bf24d862d7526ef5da77d0a6
George <golubev_georgiy@ukr.net>
2023-03-06 14:40:29 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -2,9 +2,6 @@ module.exports = (data) => {
     
     return new Promise((resolve, reject) => {
     data.hello = "Hello World!" + data.in;
-          
-        if (response) {
-        data.answer = response;
-        resolve(data);
-      }
+    resolve(data);
+     }
 };
