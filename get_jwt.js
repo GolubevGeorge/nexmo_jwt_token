680555fc72c951ec7e35e718aad8f0b7292eab1d
George <golubev_georgiy@ukr.net>
2023-03-06 16:34:51 +0200

    Update get_jwt.js

--- get_jwt.js
+++ get_jwt.js
@@ -1,9 +1,6 @@
 'use strict';
 
 module.exports.Get_JWT = function(secret, defaults) {
-    if (secret == "x") {
-        return secret + " !!!!";
-    } else {
-        return "Error";
-    }
+ 
+        return secret + "FUUUUUUUCK!!!!";
 }
