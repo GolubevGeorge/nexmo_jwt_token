29f51c126059774c432da720ad769a0949e93313
George <golubev_georgiy@ukr.net>
2023-03-09 16:23:33 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const str = require('get_jwt');
+
+var res1 = str.GET_JWT("qqqqq");
+
 module.exports = (data) => {
-  data.res = "Success!" + data.x;
+  data.res = "Success!" + data.x + res1;
   return data;
 }
