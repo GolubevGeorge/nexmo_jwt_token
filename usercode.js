163f525f53fa5cc594e02eacc0352fa74f83aaaf
George <golubev_georgiy@ukr.net>
2023-03-14 18:59:35 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,9 +1,10 @@
 
+
 module.exports = (data) => {
     return new Promise((resolve, reject) => {
        
-        
-        var xhr = new XMLHttpRequest();
+        var xhr = require('xhr2');
+        xhr = new XMLHttpRequest();
         xhr.open('POST', 'https://api.nexmo.com/v1/calls');
 
         xhr.setRequestHeader('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NzQ0NzE2MjAsImV4cCI6MTY4MzAyNTIyMCwianRpIjoiQUtVQ0RZMktKNXFrIiwiYXBwbGljYXRpb25faWQiOiJjZmZjZmNhMS01NDAxLTRmNWMtOWQxZC0zNDM1NzY0NzhkOWEifQ.FZUPxMUjoTe2n91jXSUlmyyATJuu48CF5BWKbL5TNobjBc7TMI6_Z1tmwX32yQy19IGM1Zvu0T8P9E9fbRy4BAvugCPw0y5KbTcha0Z3CtNmYAEBR554TTpXnjRzfjFogsvqCfwvwHV2qUIeF21fz7KTRnG_x3RYlwzpozpV2CZCoqge7VE_cJbr0x5xuwNnxwgRCSKGdmCnWtBU4sYKSiGyOq5zsCYjoQiYiB6Ktf9QmBUG_M3wDEP5XKhf-fTdGy-c3lHrpMLIK9MerkdLjxchy9Q5R9ilBx_Uw34hXtEGln-eRWKNi4ek5HnLlUiGXTBzjVRUUUMWAud1XmWHyg');
